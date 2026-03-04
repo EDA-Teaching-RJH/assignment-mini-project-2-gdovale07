1727c5a15a705c6562d340b91ea20a37283767f9
gdovale07 <ed12@kent.ac.uk>
2026-03-04 11:06:32 +0000

    recipebook class and tracking system was created

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 def start_program():
     print("program started")
 
+class RecipeBook:
+    def __init__(self):
+        self.recipes = []
+
+start_program()
+book = RecipeBook()
+print("recipe tracker started")
+print("number of recipes:", len(book.recipes))
+
 start_program()
