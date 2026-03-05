41b3e661507304c1c4c8a992b004e733e87f02c4
gdovale07 <ed12@kent.ac.uk>
2026-03-05 10:08:04 +0000

    defining what specific names ill be using are referring to

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-def start_program():
-    print("program started")
-
 class RecipeBook:
-    def __init__(self):
+    def __init__(self, filename):
+        self.filename = filename
         self.recipes = []
-
-start_program()
-book = RecipeBook()
-print("recipe tracker started")
-print("number of recipes:", len(book.recipes))
-
-start_program()
+        self.load()
