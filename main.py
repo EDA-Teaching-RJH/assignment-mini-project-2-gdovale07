b039aa43582174ed3e96014702162762505d377d
gdovale07 <ed12@kent.ac.uk>
2026-03-09 11:08:21 +0000

    added informative comments

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ def load_recipes(filename):
     file.close()
 
     return recipes
-
 def save_recipes(filename, recipes):
     file = open(filename, "w")
     for recipe in recipes:
         file.write(recipe + "\n")
     file.close()
-
+##saves recipes to the recipe file
 class recipebook:
     def __init__(self, filename):
         self.filename = filename
@@ -37,8 +36,8 @@ class recipebook:
             print("recipe deleted")
         else:
             print("recipe not found")
-
-def main():
+## removes recipes from the database
+def main():## start of the running code
     book = recipebook("recipes.txt")
 
     while True:
@@ -48,9 +47,9 @@ def main():
         print("3) search recipes")
         print("4) delete recipes")
         print("5) exit")
-
+## main menu which contains options for user interaction
         choice = input("choose an option (1-5): ")
-
+## asks user for an input from the main menu of options
         if choice == "1":
             print("\nrecipes currently saved:")
             if len(book.recipes) == 0:
@@ -58,7 +57,7 @@ def main():
             else:
                 for recipe in book.recipes:
                     print("-", recipe)
-        
+## displays all saved recipes for user to see        
         elif choice == "2":
             recipename = input("enter recipe name: ")
 
@@ -67,7 +66,7 @@ def main():
             else:
                 book.acc.recipe(recipename.strip())
                 print("recipe saved.")
-
+## saves inputted recipes into the database
         elif choice == "3":
             searchtext = input("search for a recipe: ")
 
@@ -84,7 +83,7 @@ def main():
                 
                 if found == False:
                     print(" (no matches)")
-        
+## browses through recipe list to find user requested items        
         elif choice == "4":
             recipetodelete = input("emter the exact recipe name to delete")
 
@@ -92,11 +91,11 @@ def main():
                 print("value cannot be empty, please try again")
             else:
                 book.delete_recipe(recipetodelete.strip())
-
+## deletes recipes from the database
         elif choice == "5":
             print("goodbye!")
             break
-
+## ends main menu option and moves on to the next step
         print("recipes currently saved:")
 
         if len(book.recipes) == 0:
@@ -104,7 +103,7 @@ def main():
         else:
             for recipe in book.recipes:
                 print("-", recipe)
-
+## prints current recipes in the database, if there are none saved, the code will inform the user of that
         searchtext = input("search for a recipe or press enter to skip")
 
         if searchtext != "":
@@ -115,7 +114,7 @@ def main():
                     print(recipe)
 
         deletechoice = input("do you want to delete a recipe? y/n: ")
-
+## gives the user an option to delete any recipes
         if deletechoice.lower() == "y":
             recipetodelete = input("enter exact recipe name to delete")
             book.delete_recipe(recipetodelete)
